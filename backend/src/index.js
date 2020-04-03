a0e9759ed2adb0cc05396d1454f4ca168c3ba1e2
Andre Hoffmann <andreh998@gmail.com>
2020-04-03 13:51:36 -0300

    Front-end completo

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // com o cors, somente a origem informada poderá acessar o back
 app.use(cors({
-  origin: 'http://localhost:3333'
+  origin: 'http://localhost:3000'
 }));
 app.use(express.json()); //adicionar isso antes das rotas para poder receber requisições POST com json
 app.use(routes);
